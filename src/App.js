ba2bff3d5e96b6abe44e9be4e18e2a6533b04d89
kiana <kiana.zrb@gmail.com>
2021-12-21 15:57:48 +0330

    refactor

--- src/App.js
+++ src/App.js
@@ -1,94 +1,126 @@
-import { Default } from 'react-spinners-css';
-import Search from './components/Search'
-import Current from './components/Current'
-import Hourly from './components/Hourly'
-import Daily from './components/Daily'
-import Conditions from './components/Conditions'
-import { useEffect,useContext } from 'react'
+import { Default } from "react-spinners-css";
+import Search from "./components/Search";
+import Current from "./components/Current";
+import Hourly from "./components/Hourly";
+import Daily from "./components/Daily";
+import Conditions from "./components/Conditions";
+import { useEffect, useContext } from "react";
 import { GlobalContext } from "./context/GlobalState.js";
 
 function App() {
-  const { tempUnit,setTempUnit,data,setData } = useContext(GlobalContext);
+  const { tempUnit, setTempUnit, data, setData, city, setCity } =
+    useContext(GlobalContext);
 
   useEffect(() => {
-
-    const noPosition = () => {
-      fetchData('tehran')
-    }
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         position => {
-          fetchDataByPostion(position.coords.latitude, position.coords.longitude)
+          fetchDataByPostion(
+            position.coords.latitude,
+            position.coords.longitude
+          );
         },
         error => {
-          console.error(error)
-          noPosition()
-        }, { enableHighAccuracy: true });
+          console.error(error);
+          setCity("tehran");
+        },
+        { enableHighAccuracy: true }
+      );
+    } else {
+      setCity("tehran");
     }
-    else {
-      noPosition()
-    }
-  }, [])
+  }, []);
 
-  const fetchData = (city) => {
-    fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
-        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+  useEffect(() => {
+    console.log("3", city);
+    if (city) fetchData(city);
+  }, [city]);
+
+  const fetchData = city => {
+    fetch(
+      `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+        },
       }
-    })
+    )
       .then(res => res.json())
       .then(res => {
-        setData(res)
+        setData(res);
       })
       .catch(err => {
         console.error(err);
-      })
-  }
+      });
+  };
   const fetchDataByPostion = (lat, long) => {
-    fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${lat}%2C${long}&days=3`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
-        "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+    fetch(
+      `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${lat}%2C${long}&days=3`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+        },
       }
-    })
+    )
       .then(res => res.json())
       .then(res => {
-        setData(res)
-      })
-  }
- 
+        setData(res);
+      });
+  };
+
   if (data)
     return (
-      <div className='app'>
-        <div className='background'>
+      <div className="app">
+        <div className="background">
           <header>
             <Search fetchData={fetchData} />
-            <div className='temp-unit'>
-              <button className={tempUnit === 'f' ? 'active' : ''} onClick={() => setTempUnit('f')}>F</button>
-              <button className={tempUnit === 'c' ? 'active' : ''} onClick={() => setTempUnit('c')}>C</button>
+            <div className="temp-unit">
+              <button
+                className={tempUnit === "f" ? "active" : ""}
+                onClick={() => setTempUnit("f")}
+              >
+                F
+              </button>
+              <button
+                className={tempUnit === "c" ? "active" : ""}
+                onClick={() => setTempUnit("c")}
+              >
+                C
+              </button>
             </div>
           </header>
 
           <Current />
         </div>
 
-        <div className='data'>
+        <div className="data">
           <Hourly />
-          <div className='flex-wrapper'>
+          <div className="flex-wrapper">
             <Daily />
             <Conditions />
           </div>
         </div>
-
       </div>
     );
   else
-    return <div style={{ height:'100vh', display:'flex',justifyContent:'center',alignItems:'center' }}>
-    <Default size={100} />
-    </div>
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Default size={100} />
+      </div>
+    );
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,61 +1,74 @@
-import React, { useContext, useState } from 'react'
-import { FaSearch,FaTimes } from 'react-icons/fa'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext, useState } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
+import { GlobalContext } from "../context/GlobalState";
 
-const Search = ({ fetchData }) => {
+const Search = () => {
+  const [text, setText] = useState("");
+  const [matches, setMatches] = useState([]);
+  const [border, setBorder] = useState(false);
+  const { setCity, data } = useContext(GlobalContext);
 
-    const [matches, setMatches] = useState([])
-    const [clas, setClas] = useState('no-border')
-    const { city,setCity, data } = useContext(GlobalContext)
+  const handleChange = text => {
+    if (text.length > 3) {
+      fetch(`https://weatherapi-com.p.rapidapi.com/search.json?q=${text}`, {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+        },
+      })
+        .then(res => res.json())
+        .then(res => {
+          setMatches(res);
+        });
+    } else setMatches([]);
+    setText(text);
+  };
+  const handleClick = e => {
+    setCity(e.target.textContent);
+    setMatches([]);
+    setText("");
+  };
+  const handleBlur = e => {
+    setText("");
+    setTimeout(() => {
+      setMatches([]);
+    }, 200);
+  };
 
-    const handleChange = (text) => {
-        if (text.length > 3) {
-          fetch(`https://weatherapi-com.p.rapidapi.com/search.json?q=${text}`, {
-            "method": "GET",
-            "headers": {
-              "x-rapidapi-key": "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
-              "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
-            }
-          })
-            .then(res => res.json())
-            .then(res => {
-              setMatches(res)
-            })
-        }
-        else setMatches([]) 
-        setCity(text)
-      }
-      const handleClick = (e) => {
-        setCity(e.target.textContent)
-        fetchData(e.target.textContent)
-        setMatches([])
-        setTimeout(() => {
-          setCity('')
-        }, 2000);
-      }
-      const handleBlur = (e) => {
-        setCity('')
-        setTimeout(() => {
-          setMatches([])
-        }, 200);
-      }
+  return (
+    <div className="search-container">
+      <button
+        className="search-button"
+        onClick={() => setBorder(border=> !border)}
+      >
+        <FaSearch className="icon" size={15} />
+      </button>
+      <div className="search">
+        <input
+          type="text"
+          value={text}
+          placeholder={`${data.location.name}, ${data.location.country}`}
+          className={border ? "border" : "no-border"}
+          onChange={e => handleChange(e.target.value)}
+          onBlur={() => handleBlur()}
+        />
+        <button className="clear-button" onClick={() => handleBlur()}>
+          <FaTimes size={13} />
+        </button>
+        {matches && (
+          <div className="matches-container">
+            {matches.map((m, i) => (
+              <div onClick={e => handleClick(e)} key={i}>
+                {m.name}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='search-container' >
-            <button className='search-button' onClick={() => setClas(clas==='border' ? 'no-border' : 'border')} >
-                <FaSearch className='icon' size={15} />
-            </button>
-            <div className='search' >
-                <input type='text' value={city} placeholder={`${data.location.name}, ${data.location.country}`}
-                      className={clas} onChange={e => handleChange(e.target.value)} onBlur={()=>handleBlur()} />
-                <button className='clear-button' onClick={()=>handleBlur()}><FaTimes size={13}/></button>
-                {matches && <div className='matches-container'>{matches.map((m, i) =>
-                    <div onClick={e => handleClick(e)}
-                        key={i}>{m.name}</div>)}
-                </div>}
-            </div>
-        </div>
-    )
-}
-
-export default Search
+export default Search;
