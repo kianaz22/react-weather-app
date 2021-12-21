704dad9e5a7eb47c5cacf31e5503d8cb8eaa8be4
kiana <kiana.zrb@gmail.com>
2021-12-21 15:14:42 +0330

    refactored search component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import Current from './components/Current'
 import Hourly from './components/Hourly'
 import Daily from './components/Daily'
 import Conditions from './components/Conditions'
-import { useState, useEffect,useContext } from 'react'
+import { useEffect,useContext } from 'react'
 import { GlobalContext } from "./context/GlobalState.js";
 
 function App() {
-  const { tempUnit,setTempUnit,setCity,data,setData } = useContext(GlobalContext);
-
-  // const [city, setCity] = useState('')
-  // const [data, setData] = useState('')
-  const [matches, setMatches] = useState([])
-  // const [tempUnit, setTempUnit] = useState('c')
+  const { tempUnit,setTempUnit,data,setData } = useContext(GlobalContext);
 
   useEffect(() => {
 
@@ -64,45 +59,13 @@ function App() {
         setData(res)
       })
   }
-  const handleChange = (text) => {
-    if (text.length > 3) {
-      fetch(`https://weatherapi-com.p.rapidapi.com/search.json?q=${text}`, {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-key": "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
-          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
-        }
-      })
-        .then(res => res.json())
-        .then(res => {
-          setMatches(res)
-        })
-    }
-    else setMatches([]) 
-    setCity(text)
-  }
-  const handleClick = (e) => {
-    setCity(e.target.textContent)
-    fetchData(e.target.textContent)
-    setMatches([])
-    setTimeout(() => {
-      setCity('')
-    }, 2000);
-  }
-  const handleBlur = (e) => {
-    setCity('')
-    setTimeout(() => {
-      setMatches([])
-    }, 200);
-  }
-
+ 
   if (data)
     return (
       <div className='app'>
         <div className='background'>
           <header>
-            <Search matches={matches} handleBlur={handleBlur}
-              handleChange={handleChange} handleClick={handleClick} />
+            <Search fetchData={fetchData} />
             <div className='temp-unit'>
               <button className={tempUnit === 'f' ? 'active' : ''} onClick={() => setTempUnit('f')}>F</button>
               <button className={tempUnit === 'c' ? 'active' : ''} onClick={() => setTempUnit('c')}>C</button>
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,47 @@ import React, { useContext, useState } from 'react'
 import { FaSearch,FaTimes } from 'react-icons/fa'
 import { GlobalContext } from '../context/GlobalState'
 
-const Search = ({ handleChange,matches,handleClick,handleBlur }) => {
+const Search = ({ fetchData }) => {
 
+    const [matches, setMatches] = useState([])
     const [clas, setClas] = useState('no-border')
-    const { city, data } = useContext(GlobalContext)
+    const { city,setCity, data } = useContext(GlobalContext)
+
+    const handleChange = (text) => {
+        if (text.length > 3) {
+          fetch(`https://weatherapi-com.p.rapidapi.com/search.json?q=${text}`, {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "3927819375mshd44cfb360819a63p12aeabjsn863dc422f8df",
+              "x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+            }
+          })
+            .then(res => res.json())
+            .then(res => {
+              setMatches(res)
+            })
+        }
+        else setMatches([]) 
+        setCity(text)
+      }
+      const handleClick = (e) => {
+        setCity(e.target.textContent)
+        fetchData(e.target.textContent)
+        setMatches([])
+        setTimeout(() => {
+          setCity('')
+        }, 2000);
+      }
+      const handleBlur = (e) => {
+        setCity('')
+        setTimeout(() => {
+          setMatches([])
+        }, 200);
+      }
+
     return (
         <div className='search-container' >
-            <button className='search-button' onClick={() => {clas==='border' ? setClas('no-border') : setClas('border')}} >
+            <button className='search-button' onClick={() => setClas(clas==='border' ? 'no-border' : 'border')} >
                 <FaSearch className='icon' size={15} />
             </button>
             <div className='search' >
