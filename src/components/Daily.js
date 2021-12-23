84adb1c2397988691a634c221b07e1c91a042eb9
kiana <kiana.zrb@gmail.com>
2021-12-24 01:57:47 +0330

    refactored daily & hourly

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -1,44 +1,58 @@
-import moment from 'moment'
-import React,{ useContext } from 'react'
-import { FaTint } from 'react-icons/fa'
+import moment from "moment";
+import React, { useContext } from "react";
+import { FaTint } from "react-icons/fa";
 import { GlobalContext } from "../context/GlobalState.js";
 
 const Daily = () => {
-    const { tempUnit, data } = useContext(GlobalContext)
+  const { tempUnit, data } = useContext(GlobalContext);
 
-    const next = data.forecast.forecastday.slice(1)
-    next.push(data.forecast.forecastday[1])
-    next.push(data.forecast.forecastday[0])
-    next.push(data.forecast.forecastday[2])
+  const nextDays = [
+    data.forecast.forecastday[1],
+    data.forecast.forecastday[2],
+    data.forecast.forecastday[0],
+    data.forecast.forecastday[1],
+    data.forecast.forecastday[2],
+  ];
 
-    const date = moment(data.location.localtime, 'YYYY-MM-DD HH:mm')
-   
-    return (
-        <div className='daily-container'>
-            <h2>Daily</h2>
-            <div className='daily'>
-                {next.map((n, i) =>
+  const date = moment(data.location.localtime, "YYYY-MM-DD HH:mm");
 
-                    <div className='day' key={i}>
-                        <div className='icon'>
-                            <img alt='condition' src={n.day.condition.icon} />
-                        </div>
-                        <div className='date-and-condition'>
-                            <div className='date'>{date.clone().add(i + 1, 'd').format('dddd')}</div>
-                            <div className='condition'>{n.day.condition.text}</div>
-                        </div>
-                        <div className='rain' style={{ visibility: n.day['daily_chance_of_rain'] > 0 ? 'visible' : 'hidden' }}>
-                            <FaTint className='rain' />{n.day['daily_chance_of_rain']}%
-                        </div>
-                        <div className='min-max'>
-                            <span>{Math.round(n.day[`mintemp_${tempUnit}`])}°</span>&nbsp;
-                            {Math.round(n.day[`maxtemp_${tempUnit}`])}°
-                        </div>
-                    </div>
-                )}
+  return (
+    <div className="daily-container">
+      <h2>Daily</h2>
+      <div className="daily">
+        {nextDays.map((n, i) => (
+          <div className="day" key={i}>
+            <div className="icon">
+              <img alt="condition" src={n.day.condition.icon} />
             </div>
-        </div>
-    )
-}
+            <div className="date-and-condition">
+              <div className="date">
+                {date
+                  .clone()
+                  .add(i + 1, "d")
+                  .format("dddd")}
+              </div>
+              <div className="condition">{n.day.condition.text}</div>
+            </div>
+            <div
+              className="rain"
+              style={{
+                visibility:
+                  n.day["daily_chance_of_rain"] > 0 ? "visible" : "hidden",
+              }}
+            >
+              <FaTint className="rain" />
+              {n.day["daily_chance_of_rain"]}%
+            </div>
+            <div className="min-max">
+              <span>{Math.round(n.day[`mintemp_${tempUnit}`])}°</span>&nbsp;
+              {Math.round(n.day[`maxtemp_${tempUnit}`])}°
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Daily
+export default Daily;
--- src/components/Hourly.js
+++ src/components/Hourly.js
@@ -1,51 +1,64 @@
-import moment from 'moment'
-import { FaChevronRight, FaChevronLeft } from 'react-icons/fa'
-import { useContext } from 'react'
+import moment from "moment";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState.js";
 
 const Hourly = () => {
-    const { tempUnit, data } = useContext(GlobalContext)
+  const { tempUnit, data } = useContext(GlobalContext);
 
-    const hours = data.forecast.forecastday[0]['hour']
-    const hours2 = data.forecast.forecastday[1]['hour']
+  const convertToNumber = time => parseInt(moment(time, "YYYY-MM-DD HH:mm").format("HH"));
 
-    const now = moment(data.location.localtime, 'YYYY-MM-DD HH:mm').format('HH')
+  const timeCheck = i => {
+    if (i === now) return "Now";
+    else if (i === 0) return "12AM";
+    else if (i === 12) return "12PM";
+    else if (i < 12) return i + "AM";
+    else if (i > 12) return i - 12 + "PM";
+  };
 
-    const scrollRight = () => {
-        if (document.querySelector('.hourly')) {
-            const hourly = document.querySelector('.hourly')
-            hourly.scrollLeft += 450
-        }
+  const now = convertToNumber(data.location.localtime);
+  const hoursToday = data.forecast.forecastday[0]["hour"].slice(now);
+  const hoursTomorrow = data.forecast.forecastday[1]["hour"].slice(0, now);
+  const hours = [...hoursToday, ...hoursTomorrow];
+
+  const scrollRight = () => {
+    if (document.querySelector(".hourly")) {
+      const hourly = document.querySelector(".hourly");
+      hourly.scrollLeft += 450;
     }
-    const scrollLeft = () => {
-        if (document.querySelector('.hourly')) {
-            const hourly = document.querySelector('.hourly')
-            hourly.scrollLeft -= 450
-        }
+  };
+  const scrollLeft = () => {
+    if (document.querySelector(".hourly")) {
+      const hourly = document.querySelector(".hourly");
+      hourly.scrollLeft -= 450;
     }
-    return (
+  };
 
-        <div className='hourly-container'>
-            <div className='scroll-container'>
-                <button onClick={scrollLeft}><FaChevronLeft /></button>
-                <div className='hourly'>
-                    {hours.map((hr, i) =>
-                        <div className='hour' key={i}>
-                            <div><img alt='condition' src={hr.condition.icon} /></div>
-                            <div className='temp'>{Math.round(hr[`temp_${tempUnit}`])}°</div>
-                            <div className='time'>{i === now ? 'Now' : (i === 12 ? '12PM' : (i < 12 ? i + 'AM' : (i - 12) + 'PM'))}</div>
-                        </div>).filter((hr, i) => i >= now)}
-                    {hours2.map((hr, i) =>
-                        <div className='hour' key={i}>
-                            <div><img alt='condition' src={hr.condition.icon} /></div>
-                            <div className='temp'>{Math.round(hr[`temp_${tempUnit}`])}°</div>
-                            <div className='time'>{i === 12 ? '12PM' : (i === 0 ? '12AM' : (i < 12 ? i + 'AM' : (i - 12) + 'PM'))}</div>
-                        </div>).filter((hr, i) => i < now)}
-                </div>
-                <button onClick={scrollRight}><FaChevronRight /></button>
+  return (
+    <div className="hourly-container">
+      <div className="scroll-container">
+        <button onClick={scrollLeft}>
+          <FaChevronLeft />
+        </button>
+        <div className="hourly">
+          {hours.map((hr, i) => (
+            <div className="hour" key={i}>
+              <div>
+                <img alt="condition" src={hr.condition.icon} />
+              </div>
+              <div className="temp">{Math.round(hr[`temp_${tempUnit}`])}°</div>
+              <div className="time">
+                {timeCheck(convertToNumber(hr.time))}
+              </div>
             </div>
+          ))}
         </div>
-    )
-}
+        <button onClick={scrollRight}>
+          <FaChevronRight />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Hourly
+export default Hourly;
